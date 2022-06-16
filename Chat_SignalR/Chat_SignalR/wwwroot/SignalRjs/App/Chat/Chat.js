94998993897ad4f0c6874427a616bd6eb3f171dc
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-15 20:24:22 -0600

    Creación Chat.js
    Archivo con la lógica del lado del cliente

---
+++ Chat_SignalR/Chat_SignalR/wwwroot/SignalRjs/App/Chat/Chat.js
@@ -0,0 +1,34 @@
+﻿"use strict"
+
+var conexion = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Inhabilitar el botón de enviar hasta que se conecte
+document.getElementById("btnEnviar").disabled = true;
+
+conexion.on("RecibirMensaje", function (user, message) {
+    var li = document.createElement("li");
+    li.textContent = `${user} - ${message}`;
+
+    document.getElementById("lstMensajes").appendChild(li);
+});
+
+conexion.start().then(function () {
+    document.getElementById("btnEnviar").disabled = false;
+
+    var li = document.createElement("li");
+    li.textContent = "Bienvenido al Chat con SignalR";
+
+    document.getElementById("lstMensajes").appendChild(li);
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("btnEnviar").addEventListener("click", function (event) {
+    var usuario = document.getElementById("txtUsuario").value;
+    var mensaje = document.getElementById("txtMensaje").value;
+    conexion.invoke("EnviarMensaje", usuario, mensaje).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
